f46624658f3de841d0a0a79e68dfad9b5d7ec066
igor9n11 <ustinov9010@gmail.com>
2025-11-13 00:32:03 +0300

    vot tak poka

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+import opendatasets as od
+import numpy as np
+import pandas as pd
+url = "https://www.kaggle.com/datasets/zeesolver/family"
+od.download(url)
+df = pd.read_csv('./family/Family Guy Dataset.csv')
+print(df.head())
